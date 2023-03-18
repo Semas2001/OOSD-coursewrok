18a30ff0be8dd6ce50bc22d6a48bbd973d857cf1
Semas2001 <semasarmonaitis@gmail.com>
2023-03-18 14:13:12 +0000

    made it so that there is and SFML library

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/semas/Documents/OOSD-Coursework/Pacman.cpp" "C:/Users/semas/Documents/OOSD-Coursework/CMakeFiles/test.dir/Pacman.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
